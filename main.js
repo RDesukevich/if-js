b3742a2fc1e0d2c0632c58ae9b8b9ebf59a6114b
RDesukevich <owkdjfl>
2021-10-07 17:46:58 +0300

    Complete lesson-4

---
+++ main.js
@@ -0,0 +1,25 @@
+function sum(x) {
+    function sum1(y) {
+        return x + y
+    }
+    return sum1
+}
+
+console.log(sum(2)(5))
+
+const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+let tag = document.getElementsByTagName('p')
+for (let i = 0; i < colors.length; i++) {
+    tag[i].addEventListener("click",func());
+}
+
+function func() {
+    let color = 0;
+    return function () {
+        this.style.color = colors[color]
+        color++
+        if (color === colors.length) {
+            color = 0
+        }
+    }
+}
