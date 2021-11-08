c95185e3335f7f5b599022bec3dc8a5832e96250
RDesukevich <owkdjfl>
2021-11-08 18:42:59 +0300

    Complete by lesson-13

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
-
-if ('Непонял как проверить есть ли у меня данные') {
+if (sessionStorage.getItem('hotels')) {
+    JSON.parse(sessionStorage.getItem('hotels')).forEach((item => {
+        const img = document.createElement('img');
+        img.src = item.imageUrl;
+        document.body.append(img);
+    }))
+}
+else {
     fetch("https://fe-student-api.herokuapp.com/api/hotels/popular").then(
         (response) =>
             response.json().then((json) => {
-                sessionStorage.setItem('GET', JSON.stringify(json));
+                json.forEach((item => {
+                    const img = document.createElement('img');
+                    img.src = item.imageUrl;
+                    document.body.append(img);
+                }))
+                sessionStorage.setItem('hotels', JSON.stringify(json));
             })
     );
-}
-else {
-    console.log(sessionStorage.getItem('GET'));
 }
 
