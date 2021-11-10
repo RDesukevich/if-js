f99433a34a1e014d095087133f8f2340d9434c07
RDesukevich <owkdjfl>
2021-11-10 22:09:58 +0300

    Complete lesson-9

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
 const colors = {
-    data: ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'],
+    data: ["magenta", "cyan", "firebrick", "springgreen", "skyblue"],
     [Symbol.iterator]() {
-        const obj = this,
-            data = this.data;
-        return {
-            next() {
-                obj.current = obj.current === undefined ? 0 : obj.current + 1;
-                return {
-                    done: obj.current === data.length,
-                    value: { color: data[obj.current], index: obj.current }
-                };
-            }
-        };
-    }
-}
+        return this;
+    },
 
-const changeStyle = id => event => {
-    event.target.style.color = colors.next(id).value;
+    next() {
+        if (this.current === undefined || this.current === this.data.length) {
+            this.current = 0;
+        }
+        return {
+            value: this.data[this.current++],
+        };
+    },
 };
+
+const element1 = document.getElementById("text1");
+const element2 = document.getElementById("text2");
+const element3 = document.getElementById("text3");
+
+const changeStyle = (id) => (event) => {
+    event.target.style.color = id.next().value;
+};
+
+const pBlock = [element1, element2, element3];
+for (let i = 0; i < pBlock.length; i++) {
+    pBlock[i].addEventListener("click", changeStyle(Object.assign({}, colors)));
+}
