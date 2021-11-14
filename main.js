9181b5303b693c3303652904257b80f7baa6309e
RDesukevich <owkdjfl>
2021-11-14 15:05:09 +0300

    Lesson-14

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
-const form = document.getElementById('formElem');
+const form = document.getElementById("form");
+const img = document.getElementById("img");
+const file = document.getElementById("file");
 
-form.onsubmit = async (event) => {
+
+file.addEventListener("change", (event) => {
     event.preventDefault();
-    let submit = await fetch('https://fe-student-api.herokuapp.com/api/file', {
-        method: 'POST',
-        body: new FormData(form)
-    })
-    let result = await submit.json();
+    const files = file.files;
+    const reader = new FileReader();
+    reader.readAsDataURL(files[0]);
+    reader.onload = () => {
+        const imgUrl = reader.result;
+        img.style.backgroundImage = `url("${imgUrl}")`;
+        img.innerHTML = "";
+    };
+});
 
-    console.log(result.message);
+async function fileUpload(event) {
+    event.preventDefault();
+    const response = await fetch(
+        "https://fe-student-api.herokuapp.com/api/file",
+        {
+            method: "POST",
+            body: new FormData(form),
+        }
+    );
+    const result = await response.json();
+    if (response.ok) {
+        console.log(result);
+    } else console.log(result.message);
 }
+
+form.onsubmit = fileUpload;
